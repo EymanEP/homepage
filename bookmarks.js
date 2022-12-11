19794a4081e11a61525983244ec0737e45cab417
Eyman <88043427+EymanEP@users.noreply.github.com>
2022-12-11 16:28:53 +0100

    added the files

---
+++ bookmarks.js
@@ -0,0 +1,40 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Drive", url: "https://drive.google.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" }
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+      { name: "/r/Piracy", url: "https://www.reddit.com/r/Piracy/" },
+      { name: "/r/CrackWatch", url: "https://www.reddit.com/r/CrackWatch/" },
+      { name: "/r/LinuxCrackSupport", url: "https://reddit.com/r/LinuxCrackSupport" }
+    ],
+  },
+  {
+    title: "Learning",
+    links: [
+      { name: "FreeCodeCamp", url: "https://www.freecodecamp.org/learn/" },
+      { name: "TheOdinProject", url: "https://www.theodinproject.com/" },
+      { name:  "Fokus", url: "https://fokusapp.netlify.app/" }
+    ],
+  },
+  {
+    title: "Self-Hosted",
+    links: [
+      { name: "Portainer", url: "https://localhost:9443/#!/auth" }
+    ]
+  }
+];
